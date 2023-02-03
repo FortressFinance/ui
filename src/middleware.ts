ee52ed8ecdbca179eae39ecd6a9d3a43e50942f6
Lint Action <lint-action@samuelmeuli.com>
2023-02-03 17:38:26 +0000

    Fix code style issues with Prettier

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 
 export const config = {
   matcher: [
@@ -11,26 +11,32 @@ export const config = {
      */
     "/((?!api/|_next/|_static/|favicon/|images/|svg/|[\\w-]+\\.\\w+).*)",
   ],
-};
+}
 
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl;
+  const url = req.nextUrl
 
   // Get hostname of request (e.g. localhost:3000)
-  const hostname = req.headers.get("host") || "fortress.finance";
+  const hostname = req.headers.get("host") || "fortress.finance"
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-  const path = url.pathname;
+  const path = url.pathname
 
-  const shouldRedirectToApp = process.env.NODE_ENV === "production" && process.env.VERCEL === "1" && hostname === "fortress.finance";
-  const shouldRedirectToLandingPage = process.env.NODE_ENV === "production" && process.env.VERCEL === "1" && hostname === "app.fortress.finance";
-  
+  const shouldRedirectToApp =
+    process.env.NODE_ENV === "production" &&
+    process.env.VERCEL === "1" &&
+    hostname === "fortress.finance"
+  const shouldRedirectToLandingPage =
+    process.env.NODE_ENV === "production" &&
+    process.env.VERCEL === "1" &&
+    hostname === "app.fortress.finance"
+
   // redirect for app pages
   if (shouldRedirectToApp && path !== "/") {
-    // we show only the landing page with fortress.finance    
-    return NextResponse.redirect(`https://app.fortress.finance/${path}`);
+    // we show only the landing page with fortress.finance
+    return NextResponse.redirect(`https://app.fortress.finance/${path}`)
   }
   if (shouldRedirectToLandingPage && path === "/") {
-    return NextResponse.redirect(`https://fortress.finance`);
+    return NextResponse.redirect(`https://fortress.finance`)
   }
 }
