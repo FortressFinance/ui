78bdf0dbb5115c27eba4a9405d23c2945b33edc2
Lint Action <lint-action@samuelmeuli.com>
2023-02-09 22:16:25 +0000

    Fix code style issues with Prettier

--- src/components/Vault/VaultTable.tsx
+++ src/components/Vault/VaultTable.tsx
@@ -39,7 +39,7 @@ const VaultTable: FC<Pick<VaultProps, "type">> = ({ type }) => {
     <div className="" role="table">
       {/* Table headings */}
       <div className="" role="rowgroup">
-        <TableRow className="rounded-b-none border-b-2 border-b-pink/30 overflow-visible">
+        <TableRow className="overflow-visible rounded-b-none border-b-2 border-b-pink/30">
           <TableHeader>{vaultTitle}</TableHeader>
           <TableHeader className="text-center">APR</TableHeader>
           <TableHeader className="text-center">TVL</TableHeader>
@@ -62,9 +62,7 @@ const VaultTable: FC<Pick<VaultProps, "type">> = ({ type }) => {
                     </button>
                   </Popover.Button>
 
-                  <Transition
-                    show={open}
-                  >
+                  <Transition show={open}>
                     <Transition.Child
                       enter="transition-opacity duration-200"
                       enterFrom="opacity-0"
