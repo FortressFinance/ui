8bc8d731a4a3ce5dac3a1fee337b8ccba99aa59c
orenku <orenku@users.noreply.github.com>
2023-02-14 21:20:50 +0200

    Uk/mobile navbar menu (#24)
    * show navbar menu in mobile
    
    * Fix code style issues with Prettier
    
    * Update ApiTokenVaults to include underlying assets
    
    * Mobiel view - move connect button to Humburger menu
    
    * Fix code style issues with Prettier
    
    * Apply fixex to top-navbar - mobile view
    
    * Fix code style issues with Prettier
    
    * Update mobile menu with the right links
    
    * Fix code style issues with Prettier
    
    * remove junk from className
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,9 @@
-import { Menu } from "@headlessui/react"
+import { Menu, Transition } from "@headlessui/react"
 import dynamic from "next/dynamic"
 import Image from "next/image"
 import Link from "next/link"
-import { FC, PropsWithChildren } from "react"
+import { FC, Fragment, PropsWithChildren } from "react"
+import { RxHamburgerMenu } from "react-icons/rx"
 
 import { appLink } from "@/lib/helpers"
 
@@ -84,8 +85,39 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
 
             <div className="flex items-center">
               <NetworkSelector />
-              <ConnectWalletButton />
+              <ConnectWalletButton className="px-2 text-sm md:px-5 md:text-base" />
             </div>
+
+            {/* Mobile navigation */}
+            <Menu
+              as="div"
+              className="relative inline-block text-left md:hidden"
+            >
+              <Menu.Button>
+                <RxHamburgerMenu className="ml-3 h-8 w-8" />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 my-3 w-56 origin-top-right divide-y divide-gray-500 bg-white px-3 text-xl text-black focus:outline-none">
+                  <Menu.Item as={DropdownMenuItem} href={appLink("/yield")}>
+                    Vaults
+                  </Menu.Item>
+                  <Menu.Item
+                    as={DropdownMenuItem}
+                    href={appLink("/yield/concentrators")}
+                  >
+                    Concentrators
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </header>
 
