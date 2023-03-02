9a35683aebf094260911d7f9cd8815ee19617bc8
Lint Action <lint-action@samuelmeuli.com>
2023-03-02 21:51:06 +0000

    Fix code style issues with Prettier

--- src/components/Disclaimer/Consent.tsx
+++ src/components/Disclaimer/Consent.tsx
@@ -14,9 +14,7 @@ export const Consent: FC = () => {
   const understandDisclaimer = useConsentStorage(
     (state) => state.understandDisclaimer
   )
-  const resetConsent = useConsentStorage(
-    (state) => state.resetConsent
-  )
+  const resetConsent = useConsentStorage((state) => state.resetConsent)
   const [isOpen, setIsOpen] = useState(false)
 
   function closeModal() {
@@ -46,15 +44,42 @@ export const Consent: FC = () => {
         <PurpleModalContent>
           <div className="w-full max-w-prose text-pink-100 max-md:text-xs md:leading-relaxed">
             <p className="mb-3">
-            Fortress Finance is a web-based user interface that enables you to interact with a set of decentralised autonomous smart-contract system deployed on the Ethereum Virtual Machine-compatible blockchain networks.
+              Fortress Finance is a web-based user interface that enables you to
+              interact with a set of decentralised autonomous smart-contract
+              system deployed on the Ethereum Virtual Machine-compatible
+              blockchain networks.
             </p>
             <p className="mb-2">
-            By interacting or attempting to interact (in both cases, “interacting”) with Fortress Finance, you confirm that you not a person subject or target of any sanctions, including a person that is:
+              By interacting or attempting to interact (in both cases,
+              “interacting”) with Fortress Finance, you confirm that you not a
+              person subject or target of any sanctions, including a person that
+              is:
             </p>
-            <ul className="list-disc list-inside">
-              <li>named in any Sanctions-related list maintained by the U.S. Department of State; the U.S. Department of Commerce, including the Bureau of Industry and Security’s Entity List and Denied Persons List; or the U.S. Department of the Treasury, including the OFAC Specially Designated Nationals and Blocked Persons List, the Sectoral Sanctions Identifications List, and the Foreign Sanctions Evaders List; or any similar list maintained by any other relevant governmental authority; </li>
-              <li>located, organized or resident in a country, territory or geographical region which is itself the subject or target of any territory-wide Sanctions (a “Restricted Territory”) (currently, but not limited to the Crimea region of Ukraine, Cuba, Iran, North Korea, and Syria); or </li>
-              <li>citizen or resident of, or person subject to jurisdiction of the United States of America (including its territories: American Samoa, Guam, Puerto Rico, the Northern Mariana Islands and the U.S. Virgin Islands), and any jurisdiction in which the use of the Platform is prohibited by applicable laws or regulation.</li>
+            <ul className="list-inside list-disc">
+              <li>
+                named in any Sanctions-related list maintained by the U.S.
+                Department of State; the U.S. Department of Commerce, including
+                the Bureau of Industry and Security’s Entity List and Denied
+                Persons List; or the U.S. Department of the Treasury, including
+                the OFAC Specially Designated Nationals and Blocked Persons
+                List, the Sectoral Sanctions Identifications List, and the
+                Foreign Sanctions Evaders List; or any similar list maintained
+                by any other relevant governmental authority;{" "}
+              </li>
+              <li>
+                located, organized or resident in a country, territory or
+                geographical region which is itself the subject or target of any
+                territory-wide Sanctions (a “Restricted Territory”) (currently,
+                but not limited to the Crimea region of Ukraine, Cuba, Iran,
+                North Korea, and Syria); or{" "}
+              </li>
+              <li>
+                citizen or resident of, or person subject to jurisdiction of the
+                United States of America (including its territories: American
+                Samoa, Guam, Puerto Rico, the Northern Mariana Islands and the
+                U.S. Virgin Islands), and any jurisdiction in which the use of
+                the Platform is prohibited by applicable laws or regulation.
+              </li>
             </ul>
           </div>
           <div className="mt-4 md:mt-6">
