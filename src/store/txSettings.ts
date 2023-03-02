0ab96488ae8e5ab8b679d0166ff8fc534fc6c983
aquatic <95545650+aquaticone@users.noreply.github.com>
2023-03-02 11:46:04 -0700

    persist tx settings (#101)
    * rename file
    
    * persist tx settings

--- src/store/txSettings.ts
+++ src/store/txSettings.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 import { DEFAULT_SLIPPAGE } from "@/constant/env"
 
@@ -9,12 +10,17 @@ export interface TxSettingsStore {
   setTxDeadlineMinutes: (_txDeadlineMinutes: number) => void
 }
 
-export const useTxSettings = create<TxSettingsStore>((set, _get) => ({
-  // we store slippage tolerance as a string for better UX
-  // with the value as a number, we lose the decimal place if there are no digits following it
-  // e.g. if a user has "0.05" in the input and presses backspace twice, the input will show "0" instead of "0."
-  slippageTolerance: DEFAULT_SLIPPAGE,
-  txDeadlineMinutes: 30,
-  setSlippageTolerance: (slippageTolerance) => set({ slippageTolerance }),
-  setTxDeadlineMinutes: (txDeadlineMinutes) => set({ txDeadlineMinutes }),
-}))
+export const useTxSettings = create<TxSettingsStore>()(
+  persist(
+    (set, _get) => ({
+      // we store slippage tolerance as a string for better UX
+      // with the value as a number, we lose the decimal place if there are no digits following it
+      // e.g. if a user has "0.05" in the input and presses backspace twice, the input will show "0" instead of "0."
+      slippageTolerance: DEFAULT_SLIPPAGE,
+      txDeadlineMinutes: 30,
+      setSlippageTolerance: (slippageTolerance) => set({ slippageTolerance }),
+      setTxDeadlineMinutes: (txDeadlineMinutes) => set({ txDeadlineMinutes }),
+    }),
+    { name: "tx-settings" }
+  )
+)
