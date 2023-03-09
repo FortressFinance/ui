e88bb66562de3febfbb1bdab57fcc4eb4083cdbc
Aquatic <aquatic@skiff.com>
2023-03-09 02:22:26 -0700

    fixed precision error with parseTokenUnits

--- src/lib/helpers/parseTokenUnits.ts
+++ src/lib/helpers/parseTokenUnits.ts
@@ -1,15 +1,14 @@
 import { BigNumber } from "ethers"
 import { parseUnits } from "ethers/lib/utils.js"
 
-export function parseTokenUnits(amount = "0", decimals = 18) {
-  const amountFloat = parseFloat(amount)
-
-  return isNaN(amountFloat)
+export function parseTokenUnits(amount = "0", decimals?: number) {
+  const amountNumber = Number(amount)
+  return isNaN(amountNumber)
     ? BigNumber.from(0)
-    : parseUnits(
-        parseFloat(amount)
-          .toFixed(decimals ?? 18)
-          .toString(),
-        decimals ?? 18
-      )
+    : parseUnits(safeToFixed(amount, decimals ?? 18))
+}
+
+function safeToFixed(numberString: string, decimals: number) {
+  const re = new RegExp("^-?\\d+(?:.\\d{0," + decimals + "})?")
+  return numberString.match(re)?.[0] ?? "0"
 }
