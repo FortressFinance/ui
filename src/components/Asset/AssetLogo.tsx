4716e02159735ccc3e89a0ef1a4717e0601f6fe9
Aquatic <aquatic@skiff.com>
2023-03-08 20:33:19 -0700

    revert to icons from github

--- src/components/Asset/AssetLogo.tsx
+++ src/components/Asset/AssetLogo.tsx
@@ -7,10 +7,21 @@ import clsxm from "@/lib/clsxm"
 import useActiveChainId from "@/hooks/useActiveChainId"
 
 import { chains } from "@/components/AppProviders"
+import Spinner from "@/components/Spinner"
 
 export type AssetLogoProps = {
   className?: string
   tokenAddress?: Address
+}
+
+const TOKEN_LOGOS_API_URL =
+  "https://raw.githubusercontent.com/FortressFinance/assets/master/blockchains"
+
+const LOGOS_NETWORK_NAME: Record<string, string> = {
+  arbitrum: "arbitrum",
+  arbitrumFork: "arbitrum",
+  mainnet: "ethereum",
+  mainnetFork: "ethereum",
 }
 
 export const AssetLogo: FC<AssetLogoProps> = ({ className, tokenAddress }) => {
@@ -18,6 +29,10 @@ export const AssetLogo: FC<AssetLogoProps> = ({ className, tokenAddress }) => {
 
   const chainId = useActiveChainId()
   const [supportedChain] = chains.filter((n) => n.id === chainId)
+  const logosNetworkName = LOGOS_NETWORK_NAME[supportedChain.network]
+  const logoUri = tokenAddress
+    ? `${TOKEN_LOGOS_API_URL}/${logosNetworkName}/assets/${tokenAddress}/logo.png`
+    : undefined
 
   return (
     <div
@@ -26,18 +41,22 @@ export const AssetLogo: FC<AssetLogoProps> = ({ className, tokenAddress }) => {
         className
       )}
     >
-      {isError ? (
-        <BiErrorCircle className="col-span-full row-span-full h-full w-full fill-dark/50" />
+      {logoUri ? (
+        isError ? (
+          <BiErrorCircle className="col-span-full row-span-full h-full w-full fill-dark/50" />
+        ) : (
+          <Image
+            src={logoUri}
+            alt=""
+            className="h-full w-full"
+            onError={() => setIsError(true)}
+            width={256}
+            height={256}
+            priority
+          />
+        )
       ) : (
-        <Image
-          src={`/images/assets/${supportedChain.network}/${tokenAddress}.webp`}
-          alt=""
-          className="h-full w-full"
-          onError={() => setIsError(true)}
-          width={256}
-          height={256}
-          priority
-        />
+        <Spinner className="col-span-full row-span-full h-full w-full fill-dark/50" />
       )}
     </div>
   )
