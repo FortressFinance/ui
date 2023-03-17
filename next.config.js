2b091ec95b79d8f5f68157ebf49f363d3ba6caae
Aquatic <aquatic@skiff.com>
2023-03-16 21:10:30 -0600

    use typescript for pages/_error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -80,5 +81,5 @@ const nextConfig = {
 module.exports = withSentryConfig(
   nextConfig,
   { silent: true },
-  { hideSourcemaps: true },
-);
+  { hideSourcemaps: true }
+)
