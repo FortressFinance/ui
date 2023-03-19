1444c81263eb0be339e01c39a6d35f297329fe03
vanilla hill <andryrkt1@yahoo.fr>
2023-03-20 00:18:48 +0100

    remove extra-space and replace white

--- src/components/Toast/LoadingToast.tsx
+++ src/components/Toast/LoadingToast.tsx
@@ -10,10 +10,10 @@ export const LoadingToast: FC<TransactionToastComponentProps> = ({
   return (
     <Toast
       isVisible={isVisible}
-      className="flex items-center bg-sky-500 text-white  "
+      className="flex items-center bg-sky-500 text-white"
     >
       <div className="text-blue-500 bg-blue-100 dark:bg-blue-800 dark:text-blue-200 inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg">
-        <Spinner className="white col-span-full row-span-full h-5 w-5" />
+        <Spinner className="fill-white col-span-full row-span-full h-5 w-5" />
       </div>
       <div className="text-md ml-3 font-normal">{message}</div>
     </Toast>
