1dd696b87753dcb222d67197c5b81b9b4a0761c3
Lint Action <lint-action@samuelmeuli.com>
2023-03-19 23:15:14 +0000

    Fix code style issues with Prettier

--- src/components/Toast/ErrorToast.tsx
+++ src/components/Toast/ErrorToast.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { FC } from "react"
 import { toast } from "react-hot-toast"
 
-import { Toast,TransactionToastComponentProps } from "@/components/Toast"
+import { Toast, TransactionToastComponentProps } from "@/components/Toast"
 
 import { FortIconWarning } from "@/icons"
 
@@ -20,7 +20,7 @@ export const ErrorToast: FC<TransactionToastComponentProps> = ({
       <div className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-red-800">
         <FortIconWarning className="col-span-full row-span-full h-5 w-5 fill-white" />
       </div>
-      <div className="text-base ml-3 font-normal">
+      <div className="ml-3 text-base font-normal">
         <div className="grid grid-rows-2">
           <div>{message}</div>
           <div>
