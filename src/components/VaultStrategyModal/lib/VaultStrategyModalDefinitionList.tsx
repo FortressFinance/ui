d74d86b61c00608c1b4807a6f45d204270d2b761
Aquatic <aquatic@skiff.com>
2023-03-19 18:57:29 -0600

    fix issue from previous pr

--- src/components/VaultStrategyModal/lib/VaultStrategyModalDefinitionList.tsx
+++ src/components/VaultStrategyModal/lib/VaultStrategyModalDefinitionList.tsx
@@ -21,7 +21,7 @@ export const VaultStrategyModalDefinitionList: FC<
 > = ({ isLoading = false, items }) => {
   const definedItems = items.filter((item) => item.value !== undefined)
   return (
-    <Skeleton isLoading={!isLoading} className="block w-full">
+    <Skeleton isLoading={isLoading} className="block w-full">
       {definedItems.length > 0 ? (
         <dl className="grid grid-cols-[max-content,auto] gap-2 text-sm text-pink-50">
           {definedItems.map((item, index) => (
