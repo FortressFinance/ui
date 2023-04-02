768e1aefaba2091cc674537acd03b1d618b298a4
vanilla hill <andryrkt1@yahoo.fr>
2023-04-03 00:09:02 +0200

    remove async

--- src/lib/api/vaults/convertToApy.ts
+++ src/lib/api/vaults/convertToApy.ts
@@ -1,4 +1,4 @@
-export async function convertToApy(apr: number | undefined) {
+export function convertToApy(apr: number | undefined) {
   const compoundPeriod = 84_600 * 7 // 7 days - 1 week
   const yearInSecond = 31_556_926
   const n = yearInSecond / compoundPeriod
