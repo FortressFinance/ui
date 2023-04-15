16d5ca0d8737ecb6e00ce688db02d84eb625e9ac
Aquatic <aquatic@skiff.com>
2023-04-15 00:17:29 -0600

    override lendingPairs on arbitrumFork

--- src/constant/chainConfig/arbitrumFork.ts
+++ src/constant/chainConfig/arbitrumFork.ts
@@ -4,4 +4,5 @@ import { ChainConfig } from "@/constant/chainConfig/types"
 export const arbitrumForkConfig: ChainConfig = {
   ...arbitrumConfig,
   registryAddress: "0x31A65C6d4EB07ad51E7afc890aC3b7bE84dF2Ead",
+  lendingPairs: [],
 }
