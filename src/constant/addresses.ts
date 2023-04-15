05cd69d865749e7076c1c8005bae1f4c5e1cc300
Aquatic <aquatic@skiff.com>
2023-04-14 21:13:41 -0600

    remove unused addresses

--- src/constant/addresses.ts
+++ src/constant/addresses.ts
@@ -8,17 +8,12 @@ export const crvTriCryptoPoolAddress: Address =
   "0x960ea3e3C7FB317332d990873d354E18d7645590"
 export const crvTriCryptoTokenAddress: Address =
   "0x8e0B8c8BB9db49a46697F3a5Bb8A308e744821D2"
-export const crvTwoCryptoPoolAddress: Address =
+export const crvTwoCryptoTokenAddress: Address =
   "0x7f90122BF0700F9E7e1F688fe926940E8839F353"
-export const crvTwoCryptoTokenAddress = crvTwoCryptoPoolAddress
 export const fraxBpTokenAddress: Address =
   "0xC9B8a3FDECB9D5b218d02555a8Baf332E5B740d5"
-export const fraxBpPoolAddress = fraxBpTokenAddress
 export const ethTokenAddress: Address =
   "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-export const AddressZero: Address = "0x0000000000000000000000000000000000000000"
-export const glpManagerAddress: Address =
-  "0x3963FfC9dff443c2A94f21b129D429891E32ec18"
 export const glpRewardsDistributorAddress: Address =
   "0x5C04a12EB54A093c396f61355c6dA0B15890150d"
 export const glpTokenAddress: Address =
