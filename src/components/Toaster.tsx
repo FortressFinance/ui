5ddd588cfed07b92211d7d56d4bed5e2a3636aae
Aquatic <aquatic@skiff.com>
2023-05-04 19:51:33 -0600

    fixed responsive issues at lg/xl screen size

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -29,7 +29,7 @@ export const Toaster: FC<{ className?: string }> = ({ className }) => {
     <>
       <ToastPrimitive.Viewport
         className={clsxm(
-          "fixed inset-x-3 bottom-9 z-50 flex flex-col gap-1.5 md:left-1/2 md:right-auto md:w-full md:max-w-sm md:-translate-x-1/2 lg:bottom-auto lg:top-2.5 lg:translate-x-48",
+          "fixed inset-x-3 bottom-9 z-50 flex flex-col gap-1.5 md:left-1/2 md:right-auto md:w-full md:max-w-sm md:-translate-x-1/2 lg:bottom-auto lg:left-auto lg:right-4 lg:top-2.5 lg:translate-x-0 xl:left-1/2 xl:translate-x-48",
           className
         )}
       />
