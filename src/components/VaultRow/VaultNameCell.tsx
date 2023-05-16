a3b293290118877b769777f1a6347574d59f74fb
vanilla hill <andryrkt1@yahoo.fr>
2023-05-16 16:21:39 +0200

    change the vault name

---
+++ src/components/VaultRow/VaultNameCell.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react"
+
+import { useDoubleTokenConfig } from "@/hooks/useDoubleTokenConfig"
+
+import { AssetLogo } from "@/components/Asset"
+import { AssetDoubleLogo } from "@/components/Asset/AssetDoubleLogo"
+import { VaultName } from "@/components/VaultRow/lib"
+import { VaultRowPropsWithProduct } from "@/components/VaultRow/VaultRow"
+
+export const VaultNameCell: FC<VaultRowPropsWithProduct> = (props) => {
+  const doubleTokens = useDoubleTokenConfig()
+  const producType = props.productType
+  const vaultAddress = props.vaultAddress
+  const ybTokenAddress = props.ybTokenAddress
+  const [mainInputToken, secondInputToken] =
+    doubleTokens?.[ybTokenAddress ?? "0x"] ?? []
+
+  if (producType === "compounder") {
+    return (
+      <>
+        <AssetLogo className="flex h-12 w-12" tokenAddress={vaultAddress} />
+
+        <span className="line-clamp-2 max-lg:mr-8">
+          <VaultName {...props} />
+        </span>
+      </>
+    )
+  }
+  return (
+    <>
+      <AssetDoubleLogo
+        className="flex h-12 w-12"
+        mainTokenAddress={mainInputToken}
+        secondTokenAddress={secondInputToken}
+      />
+
+      <span className="line-clamp-2 max-lg:mr-8">
+        <VaultName {...props} vaultAddress={mainInputToken} />
+      </span>
+    </>
+  )
+}
--- src/components/VaultRow/index.ts
+++ src/components/VaultRow/index.ts
@@ -1,1 +1,2 @@
+export * from "./VaultNameCell"
 export * from "./VaultRow"
