ddd0b246c57322b084d36b3c835cd2acd4b1a807
vanilla hill <andryrkt1@yahoo.fr>
2023-05-24 00:47:46 +0200

    add custom legend

--- src/components/Modal/VaultStrategyModal/managedVaults/ManagedVaultsStrategyModalAllocations.tsx
+++ src/components/Modal/VaultStrategyModal/managedVaults/ManagedVaultsStrategyModalAllocations.tsx
@@ -2,9 +2,14 @@ import { ArcElement, Chart as ChartJS, Colors, Legend, Tooltip } from "chart.js"
 import { FC } from "react"
 import { Doughnut } from "react-chartjs-2"
 
+import { formatPercentage } from "@/lib/helpers"
+
+import { AssetLogo } from "@/components/Asset"
+
 ChartJS.register(ArcElement, Tooltip, Legend, Colors)
 
 export const ManagedVaultsStrategyModalAllocations: FC = () => {
+  const address = "0xadAD55f56C23cF8B1286A3419bFeed055F1aDcb0"
   const data = {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
@@ -12,20 +17,20 @@ export const ManagedVaultsStrategyModalAllocations: FC = () => {
         label: "# of Votes",
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(255,99,132,0.2)",
+          "rgba(54,162,235,0.2)",
+          "rgba(255,206,86,0.2)",
+          "rgba(75,192,192,0.2)",
+          "rgba(153,102,255,0.2)",
+          "rgba(255,159,64,0.2)",
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          "rgba(255,99,132,1)",
+          "rgba(54,162,235,1)",
+          "rgba(255,206,86,1)",
+          "rgba(75,192,192,1)",
+          "rgba(153,102,255,1)",
+          "rgba(255,159,64,1)",
         ],
         borderWidth: 1,
       },
@@ -35,42 +40,46 @@ export const ManagedVaultsStrategyModalAllocations: FC = () => {
   const options = {
     plugins: {
       legend: {
-        position: "bottom", // Adjust the position of the legend
-        align: "start",
-        labels: {
-          font: {
-            size: 14, // Customize the font size
-          },
-          boxWidth: 20, // Adjust the width of the legend color boxes
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          generateLabels: (chart: any) => {
-            const { data } = chart
-            if (data.labels.length && data.datasets.length) {
-              return data.labels.map((label: string, index: number) => {
-                const dataset = data.datasets[0]
-                const value = dataset.data[index]
-                return {
-                  text: `${label}: ${value}`,
-                  fontColor: "rgb(243,215,229)",
-                  fillStyle: dataset.backgroundColor[index],
-                  hidden: chart.getDatasetMeta(0).data[index].hidden,
-                  lineCap: "round",
-                  lineDash: [],
-                  lineDashOffset: 0,
-                  lineJoin: "round",
-                  lineWidth: 1,
-                  strokeStyle: dataset.borderColor[index],
-                  pointStyle: dataset.pointStyle,
-                  rotation: 0,
-                }
-              })
-            }
-            return []
-          },
-        },
+        display: false,
       },
     },
   }
 
-  return <Doughnut data={data} options={options} className="w-full" />
+  const sumTotal = data.datasets?.[0]?.data.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
+
+  return (
+    <div className="grid w-full grid-cols-1 grid-rows-[auto,1fr]">
+      <Doughnut data={data} options={options} className="w-full" />
+      <div className="grid grid-cols-[20px,auto,1fr] gap-1.5">
+        {data.labels?.map((label, i) => (
+          <>
+            <div
+              key={`legend-${i}`}
+              className="mt-1 h-4 w-4 border"
+              style={{
+                borderColor: `${data.datasets?.[0].borderColor[i]}`,
+                backgroundColor: `${data.datasets?.[0].backgroundColor[i]}`,
+              }}
+            ></div>
+            <div>
+              <div className="relative float-left mr-2 h-6 w-6 rounded-full bg-white">
+                <AssetLogo tokenAddress={address} />
+              </div>
+              <span className="text-left text-xs uppercase text-pink-50">
+                {label}
+              </span>
+            </div>
+            <span className="mt-1 text-right text-xs text-pink-50">
+              {formatPercentage(
+                (data.datasets?.[0]?.data?.[i] * 100) / sumTotal / 100
+              )}
+            </span>
+          </>
+        ))}
+      </div>
+    </div>
+  )
 }
