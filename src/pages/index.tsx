4427ca240c6bd7de792fdb44dab8ef0dd3dc35cf
Aquatic <aquatic@skiff.com>
2023-06-23 00:16:41 -0600

    opt-in ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next"
+import { NextPage } from "next"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -10,24 +10,13 @@ import FortressLogoAnimated from "~/images/fortress-animated-logo.gif"
 import SwordImage from "~/images/sword.gif"
 import FortressLogo from "~/svg/fortress-logo.svg"
 
-type Data = {
-  appUrl: string
+export const getStaticProps = () => {
+  // Automatic SSG is disabled because of getInitialProps in _app.tsx
+  // Exporting this function enables SSG for this page
+  return { props: {} }
 }
 
-export const getServerSideProps: GetServerSideProps<Data> = async (context) => {
-  const host = context.req.headers.host || "fortress.finance"
-  const appUrl =
-    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-      ? `https://${host}/app`
-      : host.includes("localhost")
-      ? `http://app.${host}`
-      : `https://app.${host}`
-  return { props: { appUrl } }
-}
-
-const HomePage: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ appUrl }) => {
+const HomePage: NextPage = () => {
   return (
     <div className="h-screen-small overflow-hidden bg-gradient-to-br from-pink to-orange p-2 lg:p-4">
       <Seo />
@@ -63,7 +52,13 @@ const HomePage: NextPage<
               </p>
               <ButtonLink
                 className="mt-6 px-8 lg:mt-8"
-                href={`${appUrl}/yield`}
+                href={`${
+                  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+                    ? "https://app.fortress.finance"
+                    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+                    ? "/app/yield"
+                    : "http://app.localhost:3000"
+                }`}
                 size="large"
               >
                 Launch dApp
