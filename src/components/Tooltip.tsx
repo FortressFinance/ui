5552ffa36033e7b42a8bfe77c478d4e39db39bbf
aquatic <95545650+aquaticone@users.noreply.github.com>
2023-07-10 09:02:39 -0600

    restored mobile tooltip functionality (#245)

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,5 +1,5 @@
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren, useState } from "react"
 
 import { inter } from "@/lib/fonts"
 
@@ -7,20 +7,32 @@ type TooltipProps = PropsWithChildren<{
   label: string
 }>
 
-const Tooltip: FC<TooltipProps> = ({ children, label }) => (
-  <TooltipPrimitive.Root>
-    <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
-    <TooltipPrimitive.Portal>
-      <TooltipPrimitive.Content
-        className={`${inter.variable} z-50 max-w-sm rounded-lg bg-blue px-4 py-2 text-center font-sans text-sm font-normal text-white shadow-md ui-state-closed:animate-fade-out ui-state-delayed-open:animate-fade-in ui-state-open:animate-fade-in`}
-        sideOffset={6}
+const Tooltip: FC<TooltipProps> = ({ children, label }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <TooltipPrimitive.Root open={isOpen}>
+      <TooltipPrimitive.Trigger
+        asChild
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        onFocus={() => setIsOpen(true)}
+        onBlur={() => setIsOpen(false)}
       >
-        {label}
-        <TooltipPrimitive.Arrow className="h-2.5 w-5 fill-blue" />
-      </TooltipPrimitive.Content>
-    </TooltipPrimitive.Portal>
-  </TooltipPrimitive.Root>
-)
+        {children}
+      </TooltipPrimitive.Trigger>
+      <TooltipPrimitive.Portal>
+        <TooltipPrimitive.Content
+          className={`${inter.variable} z-50 max-w-sm rounded-lg bg-blue px-4 py-2 text-center font-sans text-sm font-normal text-white shadow-md ui-state-closed:animate-fade-out ui-state-delayed-open:animate-fade-in ui-state-open:animate-fade-in`}
+          sideOffset={6}
+        >
+          {label}
+          <TooltipPrimitive.Arrow className="h-2.5 w-5 fill-blue" />
+        </TooltipPrimitive.Content>
+      </TooltipPrimitive.Portal>
+    </TooltipPrimitive.Root>
+  )
+}
 
 export default Tooltip
 
